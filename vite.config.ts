bf14c78746ea711d7123fc8d8a795ed3bd0044f8
Takashi Ichikihara <dev@tichikihara.com.br>
2025-05-07 12:54:59 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: "/",
+  build: {
+    outDir: "dist"
   },
+  server: {
+  }
 });
