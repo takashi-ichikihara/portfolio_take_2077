88296df5f8b69bd6a96b7cdfe19c3b386e32b07e
Takashi Ichikihara <dev@tichikihara.com.br>
2025-05-12 11:10:15 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ezlfnoygcplyemaivgts.supabase.co'; // Substitua pela sua URL do Supabase
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6bGZub3lnY3BseWVtYWl2Z3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyMzg2OTgsImV4cCI6MjA2MTgxNDY5OH0.wkT0Umx0irP4z7H_HIActn0GbKQko6t9ri_NfEgMgRI'; // Substitua pela sua anon key do Supabase
+const supabaseUrl = '#URL DO SUPABASE'; // Substitua pela sua URL do Supabase
+const supabaseAnonKey = '#SEU ANON KEY AQUI'; // Substitua pela sua anon key do Supabase
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
