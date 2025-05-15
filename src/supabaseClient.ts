2a20568de6af8f1a23f576e8d8b7dc287bb525da
Takashi Ichikihara <dev@tichikihara.com.br>
2025-05-15 05:21:28 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = https://ezlfnoygcplyemaivgts.supabase.co;
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6bGZub3lnY3BseWVtYWl2Z3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyMzg2OTgsImV4cCI6MjA2MTgxNDY5OH0.wkT0Umx0irP4z7H_HIActn0GbKQko6t9ri_NfEgMgRI;
+const supabaseUrl = VITE_SUPABASE_URL;
+const supabaseAnonKey = VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
